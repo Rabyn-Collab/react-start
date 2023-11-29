496d578fe0fa434722cfce939bdf4e655269fd92
Rabyn-Collab <rabyn900@gmail.com>
2023-11-29 07:28:07 +0545

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sample from './Sample';
+import { movie } from '../dummy/movie_data';
 
 const HomePage = () => {
-  return (
-    <div>
 
+  const [count, setCount] = useState(0);
+
+
+  const addTo = () => {
+    setCount((prev) => prev + 1);
+  }
+
+  const minusTo = () => {
+    setCount((prev) => prev - 1);
+  }
+  const movieData = movie;
+
+  const m = {
+    ji: 90
+  };
+  return (
+    <div className='p-5'>
+
+      <h1>{count}</h1>
+
+
+      <Sample some={900} add={addTo} />
     </div>
   )
 }
---
+++ src/pages/Sample.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Sample = ({ some, add }) => {
+
+  return (
+    <div>
+      <button onClick={add}>AddTo</button>
+      <h1>Sample Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor itaque molestiae harum assumenda reprehenderit aut, neque sed quae exercitationem. Sapiente inventore, numquam libero fuga delectus iste odit earum deserunt tenetur possimus error officia, vitae eius nam ipsum, sit ipsa quisquam! Maiores modi voluptatum pariatur, iusto voluptatibus quidem facilis ad nam magni distinctio incidunt ratione porro tenetur? Repellat, temporibus quidem consectetur quod officia unde harum magnam velit veritatis ad laborum accusamus esse cumque, in reprehenderit nam exercitationem, excepturi vero nisi vel? Maxime recusandae qui, cumque tempore velit enim repellat esse totam!</p>
+    </div>
+  )
+}
+
+export default Sample
