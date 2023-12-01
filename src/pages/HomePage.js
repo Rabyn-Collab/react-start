3677b6393258f9439848815d144b57900d10a288
Rabyn-Collab <rabyn900@gmail.com>
2023-12-01 08:01:45 +0545

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,57 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 
 const HomePage = () => {
-  const [n, setN] = useState(0);
+
+  const [data, setData] = useState(null);
+  const [load, setLoad] = useState(false);
+  const [err, setErr] = useState(null);
+
+
+
+
+  const getProducts = async () => {
+    try {
+      setLoad(true);
+      const response = await axios.get('https://dummyjson.com/products');
+      setLoad(false);
+      setData(response.data);
+    } catch (err) {
+      setLoad(false);
+      setErr(err.message);
+
+    }
+  }
 
   useEffect(() => {
-    if (n === 0) {
+    getProducts();
+  }, []);
 
-    } else {
-      console.log('asdl;k');
-    }
-  }, [n]);
+
+  if (load) {
+    return <h1>Loading....</h1>;
+  }
+
+  if (err) {
+    return <h1>{`${err}`}</h1>;
+  }
+
+
+  // setInterval(() => {
+  //   console.log('lio');
+  // }, 1000)
+
+  // setTimeout(() => {
+  //   console.log('lio');
+  // }, 4000)
+  // console.log('hello jee');
+
+
+  console.log(data);
+
   return (
     <div>
-      <button onClick={() => setN(n + 1)}>change to</button>
+
 
     </div>
   )
