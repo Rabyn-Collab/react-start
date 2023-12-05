e1c9f0d7ca0cbcb3ca6902128922615b3f278432
Rabyn-Collab <rabyn900@gmail.com>
2023-12-05 07:13:11 +0545

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
-
-
 const Header = () => {
+
+  const [show, setShow] = useState(false);
+
+  const navMenu = [
+    { path: '/about-page', name: 'About' },
+    { path: '/contact-page', name: 'Contact' },
+  ];
+
+
   return (
     <div className='bg-black text-white flex justify-between items-baseline px-4 py-2'>
-      <h1 className='text-2xl'>Logo</h1>
-      <nav className='space-x-5'>
-        <NavLink className={
+      <div className='flex flex-col space-y-1'>
+        <h1 className='text-2xl'>Logo</h1>
+        {show && <div className='sm:flex flex-col hidden '>
+          {navMenu.map((nav, i) => {
+            return <NavLink key={i} to={nav.path}>{nav.name}</NavLink>
+          })}
+        </div>
+        }
+
+      </div>
+
+
+      <button onClick={() => setShow(!show)} className='hidden sm:flex'> {show ? <i className="fa-solid fa-xmark fa-lg"></i> : <i className="fa-solid fa-bars fa-lg"></i>}</button>
+
+      <nav className='space-x-5 sm:hidden'>
+
+        {navMenu.map((nav, i) => {
+          return <NavLink key={i} to={nav.path}>{nav.name}</NavLink>
+        })}
+
+
+        {/* <NavLink className={
           (n) => {
             const { isActive } = n;
             return isActive === true ? 'text-pink-700' : 'text-white';
           }
         } to='/about-page' >About</NavLink>
-        <NavLink to='/contact-page'>Contact</NavLink>
+        <NavLink to='/contact-page'>Contact</NavLink> */}
 
       </nav>
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,59 +1,11 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import Display from './Display'
 
 const HomePage = () => {
-
-  const [data, setData] = useState(null);
-  const [load, setLoad] = useState(false);
-  const [err, setErr] = useState(null);
-
-
-
-
-  const getProducts = async () => {
-    try {
-      setLoad(true);
-      const response = await axios.get('https://dummyjson.com/products');
-      setLoad(false);
-      setData(response.data);
-    } catch (err) {
-      setLoad(false);
-      setErr(err.message);
-
-    }
-  }
-
-  useEffect(() => {
-    getProducts();
-  }, []);
-
-
-  if (load) {
-    return <h1>Loading....</h1>;
-  }
-
-  if (err) {
-    return <h1>{`${err}`}</h1>;
-  }
-
-
-  // setInterval(() => {
-  //   console.log('lio');
-  // }, 1000)
-
-  // setTimeout(() => {
-  //   console.log('lio');
-  // }, 4000)
-  // console.log('hello jee');
-
-
-  console.log(data);
-
   return (
-    <div>
-
-
-    </div>
+    <>
+      <Display />
+    </>
   )
 }
 
