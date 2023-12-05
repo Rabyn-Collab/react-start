ec686bd6d973566178c2846956a44d6b95557051
Rabyn-Collab <rabyn900@gmail.com>
2023-12-05 07:54:34 +0545

    changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Display from './Display'
 import Tech from './Tech'
+import Footer from './Footer'
 
 const HomePage = () => {
   return (
     <>
       <Display />
       <Tech />
+      <Footer />
     </>
   )
 }
