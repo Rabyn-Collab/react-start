f0a2be7eaffaef98f61559f41295a35d3dab0e51
Rabyn-Collab <rabyn900@gmail.com>
2023-12-08 08:00:00 +0545

    changes

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -10,11 +10,34 @@ import {
   Option,
 } from "@material-tailwind/react";
 import { useFormik } from "formik";
-
+import * as Yup from 'yup';
 
 
 const AddForm = () => {
+  // const dat = ['ram', 'shyam'];
 
+
+  // console.log(dat.includes('ram'));
+
+  const valSchema = Yup.object({
+    title: Yup.string().required(),
+    detail: Yup.string().required(),
+    place: Yup.string().required(),
+    times: Yup.array().min(1).required(),
+    country: Yup.string().required(),
+    //  country: Yup.string().matches(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'please provide valid  email').required(),
+
+    imageFile: Yup.mixed()
+
+      .test('fileType', 'Invalid file type', (value) => {
+        return value && ['image/jpeg', 'image/png', 'image/jpg'].includes(value.type);
+      })
+
+      .test('fileSize', 'File too large', (value) =>
+        value && value.size <= 4 * 1024 * 1024
+      )
+
+  });
   const formik = useFormik({
     initialValues: {
       title: '',
@@ -27,7 +50,8 @@ const AddForm = () => {
     },
     onSubmit: (val) => {
       console.log(val);
-    }
+    },
+    validationSchema: valSchema
   });
 
   const radioData = [
@@ -46,18 +70,24 @@ const AddForm = () => {
       <Typography variant="h4" color="blue-gray">
         Add Your Daily Blogs
       </Typography>
-
+      y
       <form onSubmit={formik.handleSubmit} className="mt-6 mb-2 ">
         <div className="mb-1 flex flex-col gap-6">
 
-          <Input
-            onChange={formik.handleChange}
-            size="lg"
-            label="Title"
-            type="text"
-            name="title"
-            value={formik.values.title}
-          />
+
+          <div>
+            <Input
+              onChange={formik.handleChange}
+              size="lg"
+              label="Title"
+              type="text"
+              name="title"
+              value={formik.values.title}
+            />
+            {formik.errors.title && formik.touched.title && <h1 className="mt-2 text-pink-700">{formik.errors.title}</h1>}
+
+          </div>
+
 
           <Textarea
             label="Description"
@@ -106,7 +136,11 @@ const AddForm = () => {
                   color={check.color} />;
               })}
 
+
+
+
             </div>
+            {formik.errors.times && formik.touched.times && <h1 className="mt-2 text-pink-700">{formik.errors.times}</h1>}
 
 
           </div>
@@ -117,11 +151,12 @@ const AddForm = () => {
             <Select
               onChange={(e) => formik.setFieldValue('country', e)}
               label="Select Country">
-              <Option value="nepal">Nepal</Option>
+              <Option value="nepalgmail.com">Nepal</Option>
               <Option value="india">India</Option>
               <Option value="china">China</Option>
 
             </Select>
+            {formik.errors.country && formik.touched.country && <h1 className="mt-2 text-pink-700">{formik.errors.country}</h1>}
           </div>
 
           <div>
@@ -144,9 +179,9 @@ const AddForm = () => {
               name="imageFile"
               accept="image/*"
             />
+            {formik.errors.imageFile && formik.touched.imageFile && <h1 className="mt-2 text-pink-700">{formik.errors.imageFile}</h1>}
 
-
-            {formik.values.imageUrl && <div className="mt-5 ">
+            {formik.values.imageUrl && !formik.errors.imageFile && <div className="mt-5 ">
               <img className="h-[200px]" src={formik.values.imageUrl} alt="" />
             </div>}
 
